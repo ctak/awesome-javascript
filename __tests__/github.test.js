b16271cb957317837eb52548a11cd9885230f2d2
C Tak <tak.changbeom@gmail.com>
2021-02-19 15:37:26 +0900

    Configure testing environments with Jest

---
+++ __tests__/github.test.js
@@ -0,0 +1,21 @@
+const GitHub = require('../lib/github')
+
+describe('Integration with GitBub API', () => {
+    let github
+
+    beforeAll(() => {
+        github = new GitHub({
+            accessToken: process.env.ACCESS_TOKEN,
+            baseURL: 'https://api.github.com',
+        })
+    })
+
+    test('Get a user', async () => {
+        const res = await github.getUser('ctak')
+        expect(res).toEqual(
+            expect.objectContaining({
+                login: 'ctak',
+            })
+        )
+    })
+})
